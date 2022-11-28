cce61f495b87ff14ff4bcd187ad554b3012c812f
jkaethee <jkaethee@uwaterloo.ca>
2022-11-28 16:50:17 -0500

    Change device sampling rate to match user selection

--- SSVEP_stuff/run_gui.py
+++ SSVEP_stuff/run_gui.py
@@ -6,10 +6,10 @@ import math
 
 device_name = 'Explore_84A1'
 refresh_rate = 60
-# explore = Explore()
-# explore.connect(device_name=device_name)
-# explore.set_channels(channel_mask='11001111')
-# explore.measure_imp()
+explore = Explore()
+explore.connect(device_name=device_name)
+explore.set_channels(channel_mask='11001111')
+explore.measure_imp()
 
 sg.theme('Reddit')
 # Everything inside the window
@@ -17,7 +17,7 @@ layout = [  [sg.Text(f'Mentalab Explore Device: {device_name}', font=('MS Sans S
             [sg.Text('SSVEP simulation window', font=('MS Sans Serif', 15, 'bold'))],
             [sg.Text('How long should the simulation be (seconds)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='30')],
             [sg.Text('EEG signal length to be analyzed (seconds)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='3')],
-            [sg.Text('EEG sampling rate (Hz)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='250')],
+            [sg.Text('EEG sampling rate (Hz)?', font=('MS Sans Serif', 11)), sg.Combo(['250', '500', '1000'], default_text='250')],
             [sg.Text('Top left frequency (Hz)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='12', key='top_left')],
             [sg.Text('Bottom left frequency (Hz)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='10', key='bottom_left')],
             [sg.Text('Top right frame frequency (Hz)?', font=('MS Sans Serif', 11)), sg.InputText(default_text='8.5', key='top_right')],
@@ -39,6 +39,8 @@ while True:
         ssvep_duration = int(values[0])
         signal_len = int(values[1])
         eeg_s_rate = int(values[2])
+        explore.set_sampling_rate(sampling_rate=eeg_s_rate)
+
         freq_keys = ['top_left', 'bottom_left', 'top_right', 'bottom_right']
         fr_rates = []
         for freq_key in freq_keys:
@@ -48,8 +50,8 @@ while True:
         experiment = OnlineSSVEP(refresh_rate, signal_len, eeg_s_rate, fr_rates, analysis_type)
 
         # subscribe the experiment buffer to the EEG data stream
-        # explore.stream_processor.subscribe(callback=experiment.update_buffer, topic=TOPICS.raw_ExG)
-        # explore.record_data(file_name='test', duration=ssvep_duration, file_type='csv', do_overwrite=True)
+        explore.stream_processor.subscribe(callback=experiment.update_buffer, topic=TOPICS.raw_ExG)
+        explore.record_data(file_name='test', duration=ssvep_duration, file_type='csv', do_overwrite=True)
         experiment.run_ssvep(ssvep_duration)
 
 window.close()
